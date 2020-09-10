ee25832b7c742176e147c8b59f6ae2e96989e592
forison <addoboakyeforison@gmail.com>
2020-09-10 09:46:33 -0400

    add routing foundation for controller

---
+++ controller/UserRouter.js
@@ -0,0 +1,29 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', () => {
+
+});
+
+router.get('/:id', () => {
+
+});
+
+router.post('/', () => {
+  
+});
+
+router.put('/:id', () => {
+  
+});
+
+router.patch('/:id', () => {
+  
+});
+
+router.delete('/', () => {
+  
+});
+
+export default router;
