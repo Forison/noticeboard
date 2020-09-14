04b3e1256035d0282dfd7cae6c953f367ab3b054
forison <addoboakyeforison@gmail.com>
2020-09-14 17:10:26 -0400

    adjust project structure

--- connect.js
+++ connect.js
@@ -1,4 +1,6 @@
 const { MongoClient } = require('mongodb');
+const express = require('express');
+const app = express();
 require('dotenv').config();
 
 async function main() {
@@ -6,7 +8,21 @@ async function main() {
     const client = new MongoClient(uri,{ useUnifiedTopology: true } );
     try {
         await client.connect();
+        const db = client.db('blogproject');
+        const colOne = db.collection('blog1');
+        const createIndexResults = await client.db("blog1").collection("users").createIndex({ "email": 1 }, { unique: true });
+        console.log(`Index successfully created: ${createIndexResults}`);
+        console.log(db.collection('blog1').find())
         await listDatabases(client);
+
+        // app.get('/', (req, res) => {
+        //   db.collection('users').find().toArray()
+        //     .then(results => {
+        //       console.log(results)
+        //     })
+        //     .catch(error => console.error(error))
+        // }).listen(process.env.PORT, ()=>{console.log('app running on port 3000')});
+
     } catch (e) {
         console.error(e);
     } finally {
@@ -16,7 +32,6 @@ async function main() {
 
 async function listDatabases(client) {
   databasesList = await client.db().admin().listDatabases();
-
   console.log("Databases:");
   databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 };
